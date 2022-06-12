245cb8a860d67c6b1f3f74d8ae232581972b9f0d
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-06-11 23:17:46 -0300

    Correção na chamada do mongo atlas

--- config/database.js
+++ config/database.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(
+    'mongodb+srv://todolist:todolist@todo-list-express.2i7zai2.mongodb.net/?retryWrites=true&w=majority',
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    })
     .then(() => console.log('Conectado ao MongoDB'))
     .catch((err) => console.log(err));
